eb5b7460b95b3b3034f60bcd9efe1e4b5a4ba1b3
germa <germa@GERMANJR-10>
2025-03-16 23:42:38 -0400

    Add project files.

---
+++ Aprovacion de grado/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese su número de control (matrícula): ");
+        string matricula = Console.ReadLine();
+
+        Console.Write("Ingrese su nombre: ");
+        string nombre = Console.ReadLine();
+
+        double suma = 0;
+        double nota;
+
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.Write($"Estimado/a, ingrese la calificación {i}: ");
+            nota = Convert.ToDouble(Console.ReadLine());
+            suma += nota;
+        }
+
+        double promedio = suma / 5;
+        Console.WriteLine($"\nEl promedio de notas es: {promedio:F2}");
+
+        if (promedio <= 70)
+        {
+            Console.WriteLine("Reprobado");
+        }
+        else
+        {
+            Console.WriteLine("Aprobado");
+        }
+    }
+}
+
